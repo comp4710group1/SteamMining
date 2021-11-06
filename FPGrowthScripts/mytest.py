a59bea59ce41521c8fd3abc539f3ce823143cd08
Michael Bathie <mbathie1999@gmail.com>
2021-11-06 17:56:11 -0500

    setup for fpgrowth checking

--- FPGrowthScripts/mytest.py
+++ FPGrowthScripts/mytest.py
@@ -1,6 +1,7 @@
 from fpgrowth import fpgrowth
 from pprint import pprint
 import csv
+import pandas as pd
 
 #itemSetList = [['1', '2', '3', '4', '5'],
             #['1', '3', '5'],
@@ -9,15 +10,22 @@ import csv
 
 itemSetList = []
 
-with open('./dataset/data.csv', newline='') as f:
-    reader = csv.reader(f)
-    itemSetList = list(reader)
+data = pd.read_csv('../UserScripts/game_list.csv')
+my_column = data.appID
+test = data.steamID
 
-freqItemSet, rules = fpgrowth(itemSetList, minSupRatio = 0.75, minConf = 0.5)
+#print(my_column)
+print(test)
 
-print('\n')
-pprint(rules)
-print('\n')
-pprint(freqItemSet)
-print('\n')
+#with open('./dataset/data.csv', newline='') as f:
+    #reader = csv.reader(f)
+    #itemSetList = list(reader)
+
+#freqItemSet, rules = fpgrowth(itemSetList, minSupRatio = 0.75, minConf = 0.5)
+
+#print('\n')
+#pprint(rules)
+#print('\n')
+#pprint(freqItemSet)
+#print('\n')
 
