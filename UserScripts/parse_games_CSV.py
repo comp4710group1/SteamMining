bceae193031f9c8c3ac33a0738b3472aed322a62
ColeVoelpel <66435041+ColeVoelpel@users.noreply.github.com>
2021-11-11 15:06:07 -0600

    Fixed parse_games script

--- UserScripts/parse_games_CSV.py
+++ UserScripts/parse_games_CSV.py
@@ -2,26 +2,31 @@ import csv
 import os
 
 f = open('./game_list.csv', 'r')
-csvreader = csv.reader(f)
-next(csvreader)
+#Read lines to get rid of headers
+f.readline()
+line = f.readline()
 f2 = open('./game_list_parsed.csv', 'w', newline='')
 writer = csv.writer(f2)
 
 output = {}
 #Go through game_list.csv file
-for row in csvreader:
-    steamID = row[0]
-    appID = row[1]
-    apps=[]
-    apps.append(appID)
-    #If the steamID already exists in the dictionary
-    if(steamID in output):
-        output[steamID].append(appID)
-        #TODO games exist already due to duplicate public IDs
-    # #Add new steamID to the dictionary
-    else:
-        output[steamID] = apps
+while line:
+    split = line.split(",")
+    #Checking if the read line is valid
+    if(len(split) > 1):
+        steamID = split[0]
+        appID = split[1]
+        apps=[]
+        apps.append(appID)
+        #If the steamID already exists in the dictionary
+        if(steamID in output):
+            if(appID not in output[steamID]):
+                output[steamID].append(appID)
 
+        # #Add new steamID to the dictionary
+        else:
+            output[steamID] = apps
+    line = f.readline()
 
 for key,value in output.items():
 
